f53f767d053f7375f5470101780257377e318cc4
Bohdan Kremnyi <b.kremnyi@gmail.com>
2026-09-04 19:30:35 +0200

    Load Retina menu icon representation

--- macOS/Sources/BetterMeetingApp/BrandViews.swift
+++ macOS/Sources/BetterMeetingApp/BrandViews.swift
@@ -6,10 +6,24 @@ enum BrandAssets {
         ?? NSImage(systemSymbolName: "waveform.badge.mic", accessibilityDescription: nil)!
 
     static let menuBarIcon: NSImage = {
-        let image = loadImage(named: "MenuBarIconTemplate")
-            ?? NSImage(systemSymbolName: "waveform", accessibilityDescription: nil)!
+        let pointSize = NSSize(width: 18, height: 18)
+        let image = NSImage(size: pointSize)
+
+        for filename in ["MenuBarIconTemplate.png", "MenuBarIconTemplate@2x.png"] {
+            guard
+                let url = Bundle.main.resourceURL?.appendingPathComponent(filename),
+                let representation = NSBitmapImageRep(contentsOf: url)
+            else { continue }
+
+            representation.size = pointSize
+            image.addRepresentation(representation)
+        }
+
+        guard !image.representations.isEmpty else {
+            return NSImage(systemSymbolName: "waveform", accessibilityDescription: nil)!
+        }
+
         image.isTemplate = true
-        image.size = NSSize(width: 18, height: 18)
         return image
     }()
 
